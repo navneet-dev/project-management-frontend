6a234472e6f421a9d965afcf21f3d0b4d25a4ee4
navneet-dev <navneetkmr029@gmail.com>
2026-06-04 20:06:18 +0530

    pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-// import Welcome from "./components/Welcome.jsx";
 import Home from "./pages/Home.jsx";
+import Navbar from "./components/navbar.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 // import Projects from "./pages/Projects.jsx";
 // import Tasks from "./pages/Tasks.jsx";
-import Contact from "./pages/Contact.jsx";
-import About from "./pages/About.jsx";
 
 function App() {
     return (
         <Router>
-            <nav className="flex space-x-4 p-4 bg-gray-800 text-white">
-                <Link to="/">Home</Link>
-
-                <Link to="/contact">Contacts</Link>
-                <Link to="/about">About</Link>
-            </nav>
+            <Navbar />
 
             <Routes>
                 <Route path="/" element={<Home />} />
-
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/Register.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+function Register() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <form className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10">
+                <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+                    Sign Up
+                </h2>
+
+                <input
+                    id="name"
+                    className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
+                    type="text"
+                    placeholder="Name"
+                    required
+                />
+                <input
+                    id="email"
+                    className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
+                    type="email"
+                    placeholder="Email"
+                    required
+                />
+                <input
+                    id="password"
+                    className="w-full border mt-1 bg-indigo-500/5 mb-7 border-gray-500/10 outline-none rounded py-2.5 px-3"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="new-password"
+                    required
+                />
+
+                <button className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600 transition-all active:scale-95 py-2.5 rounded text-white font-medium">
+                    Create Account
+                </button>
+
+                <p className="text-center mt-4">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-500 underline">
+                        Log In
+                    </Link>
+                </p>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
