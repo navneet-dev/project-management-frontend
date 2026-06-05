d8da8d5da42d0ffab243192d11be0d5ae5804839
navneet-dev <navneetkmr029@gmail.com>
2026-06-05 16:05:10 +0530

    auth pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import instance from "../axios.js";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle login logic here
+        // console.log({ email, password });
+        setLoading(true);
+        try {
+            const response = await instance.post("/login", {
+                email,
+                password,
+            });
+            // console.log("Login successful:", response.data.message);
+            const token = response.data.access_token;
+            localStorage.setItem("token", token);
+            // console.log("User data:", response.data.user);
+
+            setMessage("Login successful!");
+            // You can redirect the user or perform other actions here
+        } catch (error) {
+            // console.error(
+            //     "Login failed:",
+            //     error.response?.data || error.message,
+            // );
+            setMessage(
+                error.response?.data?.message ||
+                    "Login failed. Please try again.",
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <form className="bg-white text-gray-500 max-w-[350px] mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
+            <form
+                className="bg-white text-gray-500 max-w-[350px] mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10"
+                onSubmit={handleSubmit}
+            >
                 <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
                     Login Now
                 </h2>
@@ -12,6 +53,8 @@ function Login() {
                     className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
                     type="email"
                     placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <input
@@ -19,6 +62,8 @@ function Login() {
                     className="w-full border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
                     type="password"
                     placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                 />
                 <div className="text-right py-4 hidden">
@@ -29,9 +74,13 @@ function Login() {
                 <button
                     type="submit"
                     className="w-full my-3 bg-indigo-500 hover:bg-indigo-600/90 active:scale-95 transition py-2.5 rounded-full text-white"
+                    disabled={loading}
                 >
-                    Log in
+                    {loading ? "Logging in..." : "Log in"}
                 </button>
+                {message && (
+                    <p className="text-red-500 text-center mt-2">{message}</p>
+                )}
                 <p className="text-center mt-4">
                     Don’t have an account?{" "}
                     <Link to="/register" className="text-blue-500 underline">
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import instance from "../axios.js";
+import { useState } from "react";
 
 function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle registration logic here
+        // You can collect form data and send it to the backend using instance.post("/register", { ... })
+        setLoading(true);
+        try {
+            const response = await instance.post("/register", {
+                name,
+                email,
+                password,
+            });
+            setMessage("Registration successful!");
+            // You can redirect the user or perform other actions here
+            navigate("/login");
+        } catch (error) {
+            // console.error(
+            //     "Registration failed:",
+            //     error.response?.data?.errors?.email[0] || error.message,
+            // );
+            setMessage(
+                error.response?.data?.message ||
+                    "Registration failed. Please try again.",
+            );
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div className="flex items-center justify-center h-screen">
-            <form className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10">
+            <form
+                className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10"
+                onSubmit={handleSubmit}
+            >
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
                     Sign Up
                 </h2>
@@ -13,6 +55,8 @@ function Register() {
                     className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
                     type="text"
                     placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     required
                 />
                 <input
@@ -20,6 +64,7 @@ function Register() {
                     className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
                     type="email"
                     placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <input
@@ -28,12 +73,21 @@ function Register() {
                     type="password"
                     placeholder="Password"
                     autoComplete="new-password"
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                 />
 
-                <button className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600 transition-all active:scale-95 py-2.5 rounded text-white font-medium">
-                    Create Account
+                <button
+                    className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600 transition-all active:scale-95 py-2.5 rounded text-white font-medium"
+                    disabled={loading}
+                    type="submit"
+                >
+                    {loading ? "Creating Account..." : "Create Account"}
                 </button>
+
+                {message && (
+                    <p className="text-red-500 text-center mt-2">{message}</p>
+                )}
 
                 <p className="text-center mt-4">
                     Already have an account?{" "}
